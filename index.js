a918ee3535a98460ef6ef1d4191a00eaa5acf567
YuguMugu <yusufmukhsinov@gmail.com>
2025-05-14 21:35:17 -0500

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
+const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Serve static homepage
+app.use(express.static('static'));
+
+// Proxy middleware: forward requests starting with /proxy
+app.use('/proxy', (req, res, next) => {
+  const target = req.query.url;
+  if (!target) return res.status(400).send('Missing url query parameter');
+
+  // Proxy the target URL, rewrite path to '/'
+  createProxyMiddleware({
+    target,
+    changeOrigin: true,
+    pathRewrite: {'^/proxy': ''},
+    onError: (err, req, res) => {
+      res.status(500).send('Proxy error: ' + err.message);
+    }
+  })(req, res, next);
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Minimal proxy running on port ${PORT}`);
+});
 
